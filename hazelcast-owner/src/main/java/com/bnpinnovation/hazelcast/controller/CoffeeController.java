e3ee9426588c901bf4e96a0e3b3acbe431ca4acd
dukim <dukim@bnpinnovation.com>
2019-01-14 14:12:36 +0900

    update

--- hazelcast-owner/src/main/java/com/bnpinnovation/hazelcast/controller/CoffeeController.java
+++ hazelcast-owner/src/main/java/com/bnpinnovation/hazelcast/controller/CoffeeController.java
@@ -5,10 +5,7 @@ import com.bnpinnovation.hazelcast.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/")
@@ -21,6 +18,13 @@ public class CoffeeController {
     public Coffee coffee(@RequestParam(value = "key",defaultValue = "1") String key) throws InterruptedException {
         log.info("controller");
         return coffeeService.makeCoffee(key);
+    }
+
+    @PutMapping(value = "/coffee/{key}", consumes = "application/json")
+    public void otherCoffee(
+            @PathVariable(value = "key") String key,
+            @RequestBody String otherCoffee) {
+        coffeeService.otherCoffee(key, otherCoffee);
     }
 
 }
--- hazelcast-owner/src/main/java/com/bnpinnovation/hazelcast/service/CoffeeService.java
+++ hazelcast-owner/src/main/java/com/bnpinnovation/hazelcast/service/CoffeeService.java
@@ -4,6 +4,7 @@ import com.bnpinnovation.hazelcast.domain.Coffee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface CoffeeService {
 
     Coffee makeCoffee(String key) throws InterruptedException;
+
+    Coffee otherCoffee(String key, String otherCoffee);
 
     @Service
     @Slf4j
@@ -22,6 +25,13 @@ public interface CoffeeService {
         public Coffee makeCoffee(String id) throws InterruptedException {
             Thread.sleep(3000);
             return new Coffee("아메리카노" + id);
+        }
+
+        @Override
+        @CachePut(key = "#id")
+        public Coffee otherCoffee(String id, String otherCoffee) {
+            log.info("otherCoffee : {}", otherCoffee);
+            return new Coffee(otherCoffee);
         }
 
         @CacheEvict(allEntries = true)
